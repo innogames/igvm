c5cfd923f602f03179234fe0c941c17085afa69b
Henning Pridöhl <henning@hprid.de>
2013-02-22 11:45:52 +0100

    Function to find all available images

--- buildvm/utils/image.py
+++ buildvm/utils/image.py
@@ -1,3 +1,6 @@
+import urllib2
+import re
+
 from fabric.api import run
 from fabric.contrib import files
 
@@ -6,6 +9,14 @@ from buildvm.utils import fail_gracefully, cmd
 BASE_URL = 'http://files.innogames.de/'
 
 run = fail_gracefully(run)
+
+def get_images():
+    try:
+        image_html = urllib2.urlopen(BASE_URL, timeout=2).read()
+    except urllib2.URLError:
+        return None
+
+    return re.findall(r'<a\s+href="(.+?)\.tar\.gz"', image_html)
 
 def download_image(image):
     url = BASE_URL + image
