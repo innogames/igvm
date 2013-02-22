9d92af6ffafc78f731e899a2ec490d932891d6a5
Henning Pridöhl <henning@hprid.de>
2013-02-22 18:45:33 +0100

    Remove path hack, doesn't work reliable

--- buildvm/bin/buildold.py
+++ buildvm/bin/buildold.py
@@ -5,9 +5,6 @@ import argparse
 import sys
 import os
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..',
-    '..')))
-
 import adminapi
 from adminapi.utils import IP
 from adminapi.dataset import DatasetError, query
