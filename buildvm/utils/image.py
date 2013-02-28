d0434e9e204054c27fa5adb95dc821402346442c
Henning Pridöhl <henning@hprid.de>
2013-02-28 12:38:54 +0100

    Check md5sum before taking existing image

--- buildvm/utils/image.py
+++ buildvm/utils/image.py
@@ -1,12 +1,14 @@
 import urllib2
 import re
 
-from fabric.api import run
+from fabric.api import run, cd, settings
 from fabric.contrib import files
 
 from buildvm.utils import fail_gracefully, cmd
 
 BASE_URL = 'http://files.innogames.de/'
+PACKET_SERVER = 'packet.ig.local'
+PACKET_DIR = '/www/files.innogames.de/htdocs'
 
 run = fail_gracefully(run)
 
@@ -20,8 +22,17 @@ def get_images():
 
 def download_image(image):
     url = BASE_URL + image
-    if not files.exists(image):
+    
+    if files.exists(image):
+        local_hash = run(cmd('md5sum {0}', image)).split()[0]
+        with settings(host_string=PACKET_SERVER):
+            with cd(PACKET_DIR):
+                remote_hash = run(cmd('md5sum {0}', image)).split()[0]
+        if local_hash != remote_hash:
+            run(cmd('wget -nc {0}', url))
+    else:
         run(cmd('wget {0}', url))
+
 
 def extract_image(image, target_dir):
     run(cmd('tar xfz {0} -C {1}', image, target_dir))
