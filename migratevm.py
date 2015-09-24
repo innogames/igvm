2fcc98993a80c26f11d741d7fa9f948cfaee65ac
Kajetan Staszkiewicz <kajetan.staszkiewicz@innogames.com>
2015-09-24 12:59:49 +0000

    Make required arguments positional

--- migratevm.py
+++ migratevm.py
@@ -12,12 +12,12 @@ from adminapi.dataset import DatasetError, query
 from managevm.migratevm import migratevm
 
 parser = argparse.ArgumentParser(description='Migrate a virtual machine.')
-parser.add_argument('--dsthv',    metavar='hostname',   required=True, help='Hostname of destination hypervisor')
-parser.add_argument('--newip',    metavar='IP address',                help='IP address to move VM to, in case you migrate between segments')
-parser.add_argument('--nopuppet', action='store_true',                 help='Skip running puppet in chroot before powering up')
-parser.add_argument('--nolbdowntime', action='store_true',             help='Don\t use testtool\'s downtime feature during migration')
-parser.add_argument('--offline',  action='store_true',                 help='Force offline migration')
-parser.add_argument('guest',      metavar='hostname',                  help='Hostname of the guest system')
+parser.add_argument('--newip',        metavar='IP address',     help='IP address to move VM to, in case you migrate between segments')
+parser.add_argument('--nopuppet',     action='store_true',      help='Skip running puppet in chroot before powering up')
+parser.add_argument('--nolbdowntime', action='store_true',      help='Don\t use testtool\'s downtime feature during migration')
+parser.add_argument('--offline',      action='store_true',      help='Force offline migration')
+parser.add_argument('vm_hostname',    metavar='vm_hostname',    help='Hostname of the guest system')
+parser.add_argument('dsthv_hostname', metavar='dsthv_hostname', help='Hostname of destination hypervisor')
 
 args = vars(parser.parse_args())
 
