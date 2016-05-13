12877032a1bcbb453c7a5593d87731c648002be5
Emre Hasegeli <emre@hasegeli.com>
2016-05-13 19:43:30 +0200

    Silence debug message

--- managevm/utils/template.py
+++ managevm/utils/template.py
@@ -1,15 +1,19 @@
 import os
 from fabric.contrib.files import upload_template as _upload_template
 
-from managevm.utils import get_installdir, fail_gracefully
+from managevm.utils import fail_gracefully
 
-_template_dir = os.path.join(os.path.dirname(__file__), '..', 'templates')
-
-print "Loading templates from: %s" % _template_dir
 
 def upload_template(filename, destination, context=None):
+    template_dir = os.path.join(os.path.dirname(__file__), '..', 'templates')
+
     _upload_template(
-            filename, destination, context, backup=False, use_jinja=True,
-            template_dir=_template_dir)
+        filename,
+        destination,
+        context,
+        backup=False,
+        use_jinja=True,
+        template_dir=template_dir,
+    )
 
 upload_template = fail_gracefully(upload_template)
