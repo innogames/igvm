5cf2eaaa8112690210eb089575f56be48b8cd8df
Emre Hasegeli <emre@hasegeli.com>
2017-06-13 09:05:46 +0000

    Fix migratevm

--- igvm/utils/storage.py
+++ igvm/utils/storage.py
@@ -143,9 +143,8 @@ def netcat_to_device(host, device, tx=None):
 
 def device_to_netcat(host, device, size, listener, tx=None):
     # Using DD lowers load on device with big enough Block Size
-    host, port = listener
     host.run(
         'dd if={0} ibs=1048576 | pv -f -s {1} '
         '| /bin/nc.traditional -q 1 {2} {3}'
-        .format(device, size, host, port)
+        .format(device, size, *listener)
     )
