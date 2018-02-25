d741814ff23ce6c0664f2a98cdede7c69d0e9089
Emre Hasegeli <emre@hasegeli.com>
2018-02-25 16:48:49 +0000

    balance: Make constraints and rules safer to use
    The unnecessary base classes of them were previously accepting any
    argument.

--- igvm/balance/constraints.py
+++ igvm/balance/constraints.py
@@ -1,73 +1,38 @@
 """igvm - Balancing Constraints
 
+This module contains constraints to select hypervisors.
+
 Copyright (c) 2018, InnoGames GmbH
 """
 
 
-class Constraint(object):
-    """Base Constraint Class
-
-    This class is the base constraints class from which all concrete
-    constraints should inherit to fulfil the base contract of how to access it.
-    Within all classes the inherit from Constraint you have access to the
-    following standard attributes as well as all key, values as attributes you
-    attached in configuration.
-    """
-
-    def __init__(self, *args, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
-
-    def fulfilled(self, vm, hv):
-        """Constraint is fulfilled
-
-        Return True if constraint is fulfilled or False if not.
-
-        :param: vm: igvm.balance.models.VM object
-        :param: hv: igvm.balance.models.Hypervisor object
-
-        :return: bool
-        """
-
-        raise NotImplementedError()
-
-
-class DiskSpace(Constraint):
+class DiskSpace(object):
     """Disk Space Constraint
 
     Check if enough disk space is free on target hypervisor. Disk space is
     determined by used disk space of VM.
     """
-
-    def __init__(self, *args, **kwargs):
-        super(DiskSpace, self).__init__(*args, **kwargs)
+    def __init__(self, reserved):
+        self.reserved = reserved
 
     def fulfilled(self, vm, hv):
         return hv.get_disk_free(fast=True) - self.reserved > vm.get_disk_size()
 
 
-class Memory(Constraint):
+class Memory(object):
     """Memory Constraint
 
     Check if hypervisor has enough memory free to move desired vm there
     """
-
-    def __init__(self, *args, **kwargs):
-        super(Memory, self).__init__(*args, **kwargs)
-
     def fulfilled(self, vm, hv):
         return hv.get_memory_free(fast=True) > vm.get_memory()
 
 
-class EnsureFunctionDistribution(Constraint):
+class EnsureFunctionDistribution(object):
     """Game World / Function Distribution
 
     Ensure that redundant servers don't reside on the same hypervisor
     """
-
-    def __init__(self, *args, **kwargs):
-        super(EnsureFunctionDistribution, self).__init__(*args, **kwargs)
-
     def fulfilled(self, vm, hv):
         for hv_vm in hv.get_vms():
             if hv_vm.hostname == vm.hostname:
@@ -77,16 +42,15 @@ class EnsureFunctionDistribution(Constraint):
         return True
 
 
-class HypervisorMaxVcpuUsage(Constraint):
+class HypervisorMaxVcpuUsage(object):
     """Hypervisor Max vCPU usage
 
     Checks the maximum vCPU usage (95 percentile) of the given hypervisor for
     the given time_range and dismisses it as target when it is over the value
     of threshold.
     """
-
-    def __init__(self, *args, **kwargs):
-        super(HypervisorMaxVcpuUsage, self).__init__(*args, **kwargs)
+    def __init__(self, threshold):
+        self.threshold = threshold
 
     def fulfilled(self, vm, hv):
         return hv.get_max_vcpu_usage() < self.threshold
--- igvm/balance/rules.py
+++ igvm/balance/rules.py
@@ -1,78 +1,32 @@
 """igvm - Balancing Rules
 
+This module contains rules to select hypervisors.  Rules are to make some
+hypervisors more preferred.  They must return a float as a ranking value for
+the hypervisor.  Where more is better.  If in doubt, you can return
+a percentage value.  It is safe to mix up certain rules without fucking up
+weight of multiple rules because each rule ranking is evaluate and best
+hypervisor gets the maximum amount of points for this round see down under
+example.
+
 Copyright (c) 2018, InnoGames GmbH
 """
 
 
-class Rule(object):
-    """Base Rule Class
-
-    This class is the base rule class from which all concrete rules should
-    inherit to fulfil the base contract of how to access it. Within all classes
-    the inherit from Constraint you have access to the following standard
-    attributes as well as all key, values as attributes you attached in
-    configuration.
-
-    :param: name: speaking name of constraint from config.json
-    :param: weight: weight by specifying a factor such as e.g. 0.25 or 2
-
-    :return:
-    """
-
-    def __init__(self, *args, **kwargs):
-        for key, value in kwargs.iteritems():
-            setattr(self, key, value)
-
-    def score(self, vm, hv):
-        """Get score the current selection
-
-        Must return a float as a ranking value for the hypervisor. Where more
-        is better and less is not so good. If in doubt you can return a
-        percentage value. It is safe to mix up certain rules without fucking up
-        weight of multiple rules because each rule ranking is evaluate and best
-        hypervisor gets the maximum amount of points for this round see down
-        under example.
-
-        :param: vm: igvm.balance.models.VM object
-        :param: hv: igvm.balance.models.Hypervisor object
-
-        :return: float
-
-            aw-hv-100 => 97.0 => 2 points
-            aw-hv-102 => 98.0 => 1 point
-
-        """
-
-        raise NotImplementedError()
-
-
-class HypervisorMaxCpuUsage(Rule):
+class HypervisorMaxCpuUsage(object):
     """Hypervisor maximum CPU usage of the last 24h with 95 percentile score
 
     Evaluates the maximum CPU usage of the last 24h and returns a score for it.
-
-    :return: score
     """
-
-    def __init__(self, *args, **kwargs):
-        super(HypervisorMaxCpuUsage, self).__init__(*args, **kwargs)
-
     def score(self, vm, hv):
         return 100.0 - (hv.get_max_cpu_usage() or 100.0)
 
 
-class CpuOverAllocation(Rule):
+class CpuOverAllocation(object):
     """CPU Over Allocation
 
     Rate the CPU over allocation of the target hypervisor and if it better or
     worse than the current one.
-
-    :return:
     """
-
-    def __init__(self, *args, **kwargs):
-        super(CpuOverAllocation, self).__init__(*args, **kwargs)
-
     def score(self, vm, hv):
         # New VM has no xen_host attribute yet.
         if not vm['xen_host']:
